d5648b8ce0e53ee0c4ebf62c4e4e45be7a4124b9
Nachtschrecken <ferris@uldagon.com>
2022-09-04 23:49:46 +0200

    dqw

--- qrCode.py
+++ qrCode.py
@@ -5,7 +5,7 @@ def generate_qrcode(text):
 
     qr = qrcode.QRCode(
         version = 1,
-        error_correction = qrcode.constants. ERROR_CORRECT_L,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
